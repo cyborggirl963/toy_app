2a6ad02aae7e73ac5ebf0bacdbedf422c37eb545
Miranda <mdl.swimmer963@gmail.com>
2021-02-10 17:31:00 -0800

    initial draft

---
+++ __init__.py
@@ -0,0 +1,60 @@
+from Users.Miranda1.Programming.toy_app.db import get_username
+from flask import Flask, request, url_for, session, g, redirect, flash
+from werkzeug.security import check_password_hash, generate_password_hash
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+@app.route('/register',methods=['GET','POST'])
+def register():
+    #needs finishing
+    #removed return at the end with template stuff 
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        error = None
+
+        if not username:
+            error = 'Username is required.'
+        elif not password:
+            error = 'Password is required.'
+        elif get_username() is not None:
+            error = 'User {} is already registered.'.format(username)
+        flash(error)
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    #needs finishing
+    #not sure what to do about error message
+    error = None
+    if request.method == 'POST':
+        request.form['username'],
+        request.form['password']):
+        error = None
+        password = get_password() 
+        
+        if user is None:
+            error = 'Incorrect username.'
+        elif not check_password_hash(user['password'], password):
+            error = 'Incorrect password.'
+
+        if error is None:
+            session.clear()
+            session['user_id'] = user['id']
+            return redirect(url_for('my_pantry.upload'))
+
+        flash(error)
+            #error = 'Invalid username/password'
+    # the code below is executed if the request method
+    # was GET or the credentials were invalid
+    
+@app.route('/post', methods=['POST', 'GET'])
+    #to do 
+    def post():
+
+
+
+    
---
+++ db.py
@@ -0,0 +1,70 @@
+import psycopg2
+from werkzeug.security import check_password_hash, generate_password_hash
+
+import click
+from flask import current_app, g
+from flask.cli import with_appcontext
+
+def get_db():
+    if 'db' not in g:
+        g.db = psycopg2.connect("dbname=test user=postgres")
+    return g.db
+
+def new_user(username,password):
+    datab = get_db()
+    error = None
+    if datab.execute(
+            'SELECT id FROM user WHERE username = ?', (username,)
+        ).fetchone() is not None:
+            error = 'User {} is already registered.'.format(username)
+    else:
+        datab.execute(
+                'INSERT INTO user (username, password) VALUES (?, ?)',
+                (username, generate_password_hash(password))
+            )
+        datab.commit()
+
+def get_password(username):
+    datab = get_db()
+    password = datab.execute(
+            'SELECT password FROM user WHERE username = ?', (username,)
+        ).fetchone()
+    return password
+
+def add_post(post):
+    conn = get_db()
+    print(conn)
+    with conn.cursor() as cur:
+        cur.execute(
+            'INSERT INTO posts (post, author_id)'
+            ' VALUES (?, ?)',
+            (post, g.user['id'])
+        )
+    conn.commit()
+
+def retrieve_post():
+    conn = get_db()
+    with conn.cursor() as cur:
+        cur.execute(
+        'SELECT post FROM posts WHERE author_id = ?', (g.user['id'],)
+        ).fetchone()
+
+def close_db(e=None):
+    db = g.pop('db', None)
+
+    if db is not None:
+        db.close()
+
+def init_db():
+    db = get_db()
+
+    with current_app.open_resource('schema.sql') as f:
+        db.executescript(f.read().decode('utf8'))
+
+
+@click.command('init-db')
+@with_appcontext
+def init_db_command():
+    """Clear the existing data and create new tables."""
+    init_db()
+    click.echo('Initialized the database.')
